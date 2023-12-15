6a50bf4af323488a38e8eb90c306ab283abc8939
Stefan2137 <93710260+Stefan2137@users.noreply.github.com>
2023-12-15 20:39:24 +0100

    add menu

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+import Mozaik
+import compare
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
+from PyQt6 import uic
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class MyApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('C:\\Users\\kubaz\\Desktop\\Seproject-Mozaik\\Seproject-Mozaik\\menu.ui', self)
+
+        self.button1 = self.findChild(QPushButton, 'pushButton')
+        self.button2 = self.findChild(QPushButton, 'pushButton_2')
+
+        self.button1.clicked.connect(self.run_program1)
+        self.button2.clicked.connect(self.run_program2)
+
+        
+        self.mozaik_program = None  
+        self.compare_program = None  
+
+    def run_program2(self):
+        path1 = "C:\\Users\\kubaz\\Desktop\\Seproject-Mozaik\\Seproject-Mozaik\\B1.png"
+        path2 = "C:\\Users\\kubaz\\Desktop\\Seproject-Mozaik\\Seproject-Mozaik\\C2.png"
+
+       
+        self.compare_program = compare.ImageComparator(path1, path2)
+       
+
+        self.compare_program.showFullScreen()
+
+    def run_program1(self):
+        path1 = "C:\\Users\\kubaz\Desktop\\Seproject-Mozaik\\Seproject-Mozaik\\Fire.png"
+        path2 = "C:\\Users\\kubaz\Desktop\\Seproject-Mozaik\\Seproject-Mozaik\\Water.png"
+
+        
+        self.mozaik_program = Mozaik.Mozaik(path1, path2)
+        
+           
+
+        self.mozaik_program.showFullScreen()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    myApp = MyApp()
+    myApp.show()
+    sys.exit(app.exec())
