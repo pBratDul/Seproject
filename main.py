48b018c2b24a69a5945e419714f7dd253c628756
damian234527 <92489768+damian234527@users.noreply.github.com>
2023-12-21 20:29:52 +0100

    Naprawa ścieżek, tytuł okna

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from PyQt6 import uic
 class MyApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('C:\\Users\\kubaz\\Desktop\\Seproject-Mozaik\\Seproject-Mozaik\\menu.ui', self)
-
+        uic.loadUi('menu.ui', self)
+        self.setWindowTitle('Image Comparator')
         self.button1 = self.findChild(QPushButton, 'pushButton')
         self.button2 = self.findChild(QPushButton, 'pushButton_2')
 
@@ -17,24 +17,18 @@ class MyApp(QMainWindow):
 
         
         self.mozaik_program = None  
-        self.compare_program = None  
+        self.compare_program = None
+
+        self.path1 = "Fire.png"
+        self.path2 = "Water.png"
 
     def run_program2(self):
-        path1 = "C:\\Users\\kubaz\\Desktop\\Seproject-Mozaik\\Seproject-Mozaik\\B1.png"
-        path2 = "C:\\Users\\kubaz\\Desktop\\Seproject-Mozaik\\Seproject-Mozaik\\C2.png"
-
-       
-        self.compare_program = compare.ImageComparator(path1, path2)
-       
+        self.compare_program = compare.ImageComparator(self.path1, self.path2)
 
         self.compare_program.showFullScreen()
 
     def run_program1(self):
-        path1 = "C:\\Users\\kubaz\Desktop\\Seproject-Mozaik\\Seproject-Mozaik\\Fire.png"
-        path2 = "C:\\Users\\kubaz\Desktop\\Seproject-Mozaik\\Seproject-Mozaik\\Water.png"
-
-        
-        self.mozaik_program = Mozaik.Mozaik(path1, path2)
+        self.mozaik_program = Mozaik.Mozaik(self.path1, self.path2)
         
            
 
